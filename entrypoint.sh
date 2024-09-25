205e6576b6fb73da48f44a2850871c779e2f0d97
senjianlu <admin@cs.cheap>
2024-09-25 20:31:00 +0800

    fix: 修复 SSL 证书权限问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,12 @@ if [ ! -f /etc/trojan-go/certificate.crt ]; then
     echo "Trojan-Go ssl certificate /etc/trojan-go/certificate.crt is required!"
     exit 1
 fi
+chmod 755 /etc/trojan-go/certificate.crt
 if [ ! -f /etc/trojan-go/private.key ]; then
     echo "Trojan-Go ssl private key /etc/trojan-go/private.key is required!"
     exit 1
 fi
+chmod 755 /etc/trojan-go/private.key
 # 1.3 Trojan-Go 服务密码
 if [ ! -n "$TROJAN_GO_SERVICE_PASSWORD" ]; then
     echo "ENV TROJAN_GO_SERVICE_PASSWORD is required!"
@@ -38,10 +40,13 @@ fi
 caddy start --config /etc/caddy/Caddyfile
 
 # 4. 替换 Trojan-Go 配置文件内容
+# 替换域名和密码
 sed -i "s/your_password/$TROJAN_GO_SERVICE_PASSWORD/" /etc/trojan-go/config.json
 sed -i "s/your-domain-name.com/$TROJAN_GO_SERVICE_PASSWORD/" /etc/trojan-go/config.json
-sed -i "s/your_cert.crt/certificate.crt/" /etc/trojan-go/config.json
-sed -i "s/your_key.key/private.key/" /etc/trojan-go/config.json
+# 将 your_cert.crt 替换为 /etc/trojan-go/certificate.crt
+sed -i "s/your_cert.crt/\/etc\/trojan-go\/certificate.crt/" /etc/trojan-go/config.json
+# 将 your_private.key 替换为 /etc/trojan-go/private.key
+sed -i "s/your_private.key/\/etc\/trojan-go\/private.key/" /etc/trojan-go/config.json
 
 # 5. 启动 Trojan-Go
 /usr/local/bin/trojan-go -config /etc/trojan-go/config.json
