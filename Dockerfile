a41bdcceb9774320e5d37096ebd380efa62ecb65
senjianlu <admin@cs.cheap>
2024-09-25 19:31:38 +0800

    init: 首次提交

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# 镜像选择 p4gefau1t/trojan-go
+FROM p4gefau1t/trojan-go:latest
+
+# 1. 安装 Caddy
+WORKDIR /root/
+RUN wget https://github.com/caddyserver/caddy/releases/download/v2.8.4/caddy_2.8.4_linux_amd64.tar.gz
+RUN tar zxvf caddy_2.8.4_linux_amd64.tar.gz
+RUN mv caddy /usr/local/bin/
+
+# 2. 下载伪装页面
+RUN mkdir -vp /var/www/html/
+WORKDIR /var/www/html/
+RUN wget https://github.com/YaninaTrekhleb/restaurant-website/archive/refs/heads/master.zip
+RUN mv /var/www/html/restaurant-website-master/* /var/www/html/
+
+# 3. 编辑 Caddy 的配置文件
+RUN mkdir -vp /etc/caddy/
+RUN echo "localhost:80 {" > /etc/caddy/Caddyfile
+RUN echo "  root * /var/www/html" >> /etc/caddy/Caddyfile
+RUN echo "  file_server" >> /etc/caddy/Caddyfile
+RUN echo "}" >> /etc/caddy/Caddyfile
+
+# 4. 备份 Trojan-Go 的配置文件
+RUN cp /etc/trojan-go/config.json /etc/trojan-go/config.json.bak
+
+# 5. 使用新的 entrypoint.sh 启动脚本
+# 拷贝 entrypoint.sh 到容器
+COPY entrypoint.sh /root/entrypoint.sh
+ENTRYPOINT ["/bin/sh", "/root/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# 1. 判断 Trojan-Go 配置是否存在
+# 1.1 配置文件 /etc/trojan-go/config.json
+if [ ! -f /etc/trojan-go/config.json ]; then
+    echo "Trojan-Go config file /etc/trojan-go/config.json is required!"
+    exit 1
+fi
+# 1.2 SSL 证书
+if [ ! -f /etc/trojan-go/certificate.crt ]; then
+    echo "Trojan-Go ssl certificate /etc/trojan-go/certificate.crt is required!"
+    exit 1
+fi
+if [ ! -f /etc/trojan-go/private.key ]; then
+    echo "Trojan-Go ssl private key /etc/trojan-go/private.key is required!"
+    exit 1
+fi
+# 1.3 Trojan-Go 服务密码
+if [ ! -n "$TROJAN_GO_SERVICE_PASSWORD" ]; then
+    echo "ENV TROJAN_GO_SERVICE_PASSWORD is required!"
+    exit 1
+fi
+# 1.4 Trojan-Go 服务域名
+if [ ! -n "$TROJAN_GO_SERVICE_DOMAIN" ]; then
+    echo "ENV TROJAN_GO_SERVICE_DOMAIN is required!"
+    exit 1
+fi
+
+# 2. 判断 Caddy 配置是否存在
+if [ ! -f /etc/caddy/Caddyfile ]; then
+    echo "Caddy config file /etc/caddy/Caddyfile is required!"
+    exit 1
+fi
+
+# 3. 启动 Caddy
+caddy start --config /etc/caddy/Caddyfile
+
+# 4. 替换 Trojan-Go 配置文件内容
+rm -rf /etc/trojan-go/config.json
+cp /etc/trojan-go/config.json.bak /etc/trojan-go/config.json
+sed -i "s/your_password/$TROJAN_GO_SERVICE_PASSWORD/" /etc/trojan-go/config.json
+sed -i "s/your-domain-name.com/$TROJAN_GO_SERVICE_PASSWORD/" /etc/trojan-go/config.json
+sed -i "s/your_cert.crt/certificate.crt/" /etc/trojan-go/config.json
+sed -i "s/your_key.key/private.key/" /etc/trojan-go/config.json
+
+# 5. 启动 Trojan-Go
+/usr/local/bin/trojan-go -config /etc/trojan-go/config.json
